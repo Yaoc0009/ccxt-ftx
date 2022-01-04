f7ae49d07aeeb1291e39be4678eb0f86a7e86477
Yaoc0009 <yeo2octave27@gmail.com>
2022-01-05 01:13:00 +0800

    edit trade function for general orders

--- main.py
+++ main.py
@@ -30,11 +30,49 @@ def get_quote(request):
 def trade_crypto(request):
     data = request.get_json()
 
-    # create limit buy order
     try:
         #create buy market order
-        order = exchange.create_market_buy_order(data['symbol'], data['quantity'])
-        # order = exchange.create_limit_buy_order(data['symbol'], data['amount'], data['price'])
+        order = exchange.create_order(data['symbol'], data['type'], data['side'], data['amount'], data['price'], data['params'])
     except Exception as e:
         return {'ERROR': str(e)}
     return order
+
+print(dir(ccxt.exchange()))           # Python
+
+########################## CREATE ORDERS ##########################
+
+# def create_limit_order(self, symbol, side, amount, price, params={}) -> dict:
+#     return self.create_order(symbol, 'limit', side, amount, price, params)
+
+# def create_market_order(self, symbol, side, amount, price=None, params={}) -> dict:
+#     return self.create_order(symbol, 'market', side, amount, price, params)
+
+# def create_limit_buy_order(self, symbol, amount, price, params={}) -> dict:
+#     return self.create_order(symbol, 'limit', 'buy', amount, price, params)
+
+# def create_limit_sell_order(self, symbol, amount, price, params={}) -> dict:
+#     return self.create_order(symbol, 'limit', 'sell', amount, price, params)
+
+# def create_market_buy_order(self, symbol, amount, params={}) -> dict:
+#     return self.create_order(symbol, 'market', 'buy', amount, None, params)
+
+# def create_market_sell_order(self, symbol, amount, params={}) -> dict:
+#     return self.create_order(symbol, 'market', 'sell', amount, None, params)
+
+
+########################## EDIT ORDERS ##########################
+
+# def edit_limit_buy_order(self, id, symbol, *args):
+#     return self.edit_limit_order(id, symbol, 'buy', *args)
+
+# def edit_limit_sell_order(self, id, symbol, *args):
+#     return self.edit_limit_order(id, symbol, 'sell', *args)
+
+# def edit_limit_order(self, id, symbol, *args):
+#     return self.edit_order(id, symbol, 'limit', *args)
+
+# def edit_order(self, id, symbol, *args):
+#     if not self.enableRateLimit:
+#         raise ExchangeError('edit_order() requires enableRateLimit = true')
+#     self.cancel_order(id, symbol)
+#     return self.create_order(symbol, *args)
